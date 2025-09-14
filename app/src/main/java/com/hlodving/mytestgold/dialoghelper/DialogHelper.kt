b8ef0dd98dea2c7283ec9aededfcbc504c731862
Hlodving <Hlodving@yandex.ru>
2025-09-15 00:18:17 +0300

    Правки с восстановлением пароля

--- app/src/main/java/com/hlodving/mytestgold/dialoghelper/DialogHelper.kt
+++ app/src/main/java/com/hlodving/mytestgold/dialoghelper/DialogHelper.kt
@@ -10,8 +10,6 @@ import com.hlodving.mytestgold.databinding.SignDialogBinding
 
 class DialogHelper(private val act: MainActivity) {
     private val accHelper = AccountHelper(act)
-
-    // Переменная для отслеживания состояния диалога
     private var isResetPasswordState = false
 
     fun createSignDialog(index: Int) {
@@ -29,10 +27,8 @@ class DialogHelper(private val act: MainActivity) {
 
         rootDialogElement.btForgetP.setOnClickListener {
             if (isResetPasswordState) {
-                // Если уже в режиме восстановления, выполняем сброс пароля
                 setOnClickResetPassword(rootDialogElement, dialog)
             } else {
-                // Если нет, переключаем UI в режим восстановления
                 setResetPasswordState(rootDialogElement)
             }
         }
@@ -44,13 +40,17 @@ class DialogHelper(private val act: MainActivity) {
         if (index == DialogConst.SING_UP_STATE) {
             rootDialogElement.tvSingTitle.text = act.resources.getString(R.string.menu_sign_up)
             rootDialogElement.btSignUpIn.text = act.resources.getString(R.string.sign_up_action)
-            rootDialogElement.edSignAlias.visibility = View.VISIBLE // Показываем поле псевдонима
-            rootDialogElement.btForgetP.visibility = View.GONE // Скрываем для регистрации
+            rootDialogElement.edSignAlias.visibility = View.VISIBLE
+            rootDialogElement.edSignPasswordConfirm.visibility = View.VISIBLE
+            rootDialogElement.btForgetP.visibility = View.GONE
+            rootDialogElement.edSignPassword.visibility = View.VISIBLE
+            rootDialogElement.btSignUpIn.visibility = View.VISIBLE
         } else {
             rootDialogElement.tvSingTitle.text = act.resources.getString(R.string.menu_sign_in)
             rootDialogElement.btSignUpIn.text = act.resources.getString(R.string.sign_in_action)
             rootDialogElement.btForgetP.visibility = View.VISIBLE
-            rootDialogElement.edSignAlias.visibility = View.GONE // Скрываем поле псевдонима
+            rootDialogElement.edSignAlias.visibility = View.GONE
+            rootDialogElement.edSignPasswordConfirm.visibility = View.GONE // Скрываем поле подтверждения
             rootDialogElement.edSignPassword.visibility = View.VISIBLE
             rootDialogElement.btSignUpIn.visibility = View.VISIBLE
             rootDialogElement.btForgetP.text = act.resources.getString(R.string.forget_password)
@@ -62,12 +62,16 @@ class DialogHelper(private val act: MainActivity) {
         if (index == DialogConst.SING_UP_STATE) {
             val email = rootDialogElement.edSignEmail.text.toString()
             val password = rootDialogElement.edSignPassword.text.toString()
+            val passwordConfirm = rootDialogElement.edSignPasswordConfirm.text.toString()
             val alias = rootDialogElement.edSignAlias.text.toString()
 
-            // Добавлена проверка на пустые поля email и password
-            if (email.isNotEmpty() && password.isNotEmpty() && alias.isNotEmpty()) {
-                accHelper.signUpWithEmail(email, password, alias)
-                dialog.dismiss()
+            if (email.isNotEmpty() && password.isNotEmpty() && passwordConfirm.isNotEmpty() && alias.isNotEmpty()) {
+                if (password == passwordConfirm) {
+                    accHelper.signUpWithEmail(email, password, alias)
+                    dialog.dismiss()
+                } else {
+                    Toast.makeText(act, "Пароли не совпадают. Пожалуйста, проверьте введённые данные.", Toast.LENGTH_LONG).show()
+                }
             } else {
                 Toast.makeText(act, "Пожалуйста, заполните все поля", Toast.LENGTH_LONG).show()
             }
@@ -80,19 +84,16 @@ class DialogHelper(private val act: MainActivity) {
         }
     }
 
-    // Переключаем UI в режим восстановления пароля
     private fun setResetPasswordState(rootDialogElement: SignDialogBinding) {
         isResetPasswordState = true
-        rootDialogElement.edSignPassword.visibility = View.GONE // Скрываем поле пароля
-        rootDialogElement.btSignUpIn.visibility = View.GONE // Скрываем кнопку "Войти"
-        // Меняем текст на кнопке "Забыли пароль?"
+        rootDialogElement.edSignPassword.visibility = View.GONE
+        rootDialogElement.edSignPasswordConfirm.visibility = View.GONE // Скрываем поле подтверждения
+        rootDialogElement.btSignUpIn.visibility = View.GONE
         rootDialogElement.btForgetP.text = act.resources.getString(R.string.restore_password)
-        // Использование строкового ресурса вместо хардкода
         rootDialogElement.tvSingTitle.text = act.resources.getString(R.string.reset_password_title)
         rootDialogElement.tvDialogMessage.visibility = View.VISIBLE
     }
 
-    // Обновлённый метод для сброса пароля
     private fun setOnClickResetPassword(rootDialogElement: SignDialogBinding, dialog: AlertDialog) {
         if (rootDialogElement.edSignEmail.text.isNotEmpty()) {
             act.mAuth.sendPasswordResetEmail(rootDialogElement.edSignEmail.text.toString())
